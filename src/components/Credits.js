9bbde6ce5f662f65a389d8433cb8ce7d949a7051
Hayley Robinson <hayley.robinson63@myhunter.cuny.edu>
2021-11-11 18:15:45 -0500

    initial credits page

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -1,3 +1,15 @@
 import React, {Component} from 'react';
 import AccountBalance from './AccountBalance';
 import {Link} from 'react-router-dom';
+
+class Credits extends Component {
+    render(){
+        return (
+            <div>
+            <h1>Credits</h1>
+            <Link to="/">Return to Home</Link>
+        </div>
+        )}
+}
+
+export default Credits;
