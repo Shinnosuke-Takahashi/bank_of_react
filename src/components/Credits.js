d1f1047812bfa85d55fefc67ef2dd019b6132a46
Shinnosuke-Takahashi <shinnosuketakahashisemail@gmail.com>
2021-11-12 16:45:29 -0500

    updated debit and credits page to show details

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -2,14 +2,31 @@ import React, {Component} from 'react';
 import AccountBalance from './AccountBalance';
 import {Link} from 'react-router-dom';
 
-class Credits extends Component {
-    render(){
-        return (
-            <div>
-            <h1>Credits</h1>
+const Credits = (props) => {
+    let creditsView = () => {
+        const { credits } = props;
+        return credits.map((credit) => {
+            let date  = credit.date.slice(0,10);
+            return  <li key={credit.id}>{credit.amount} {credit.description} {date}</li>
+        })
+    }
+    return (
+        <div>
+            <h1> Credits </h1>
+            {creditsView()}
+
+            <Link to="/userProfile">User Profile</Link>
+            <h1></h1>
+            <Link to="/LogIn">Log In</Link>
+            <h1></h1>
+            <Link to="Credits">Credits</Link>
+            <h1></h1>
             <Link to="/">Return to Home</Link>
+            <h1></h1>
+
+            
         </div>
-        )}
+    )
 }
 
 export default Credits;
--- src/components/Debits.js
+++ src/components/Debits.js
@@ -2,16 +2,31 @@ import React, {Component} from 'react';
 import AccountBalance from './AccountBalance';
 import {Link} from 'react-router-dom';
 
-class Debits extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Debits</h1>
-
-                <Link to="/">Return to Home</Link>
-            </div>
-        );
+const Debits = (props) => {
+    let debitsView = () => {
+        const { debits } = props;
+        return debits.map((debit) => {
+            let date = debit.date.slice(0,10);
+            return <li key={debit.id}>{debit.amount} {debit.description} {date}</li>
+        })
     }
+    return (
+        <div>
+            <h1> Debits </h1>
+            {debitsView()}
+
+            <Link to="/userProfile">User Profile</Link>
+            <h1></h1>
+            <Link to="/LogIn">Log In</Link>
+            <h1></h1>
+            <Link to="Credits">Credits</Link>
+            <h1></h1>
+            <Link to="/">Return to Home</Link>
+            <h1></h1>
+
+            
+        </div>
+    )
 }
 
 export default Debits;
